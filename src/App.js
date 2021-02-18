b95a70db3a0f961f60da0a405818ee0dfd6b1a73
Simon K. Mortensen <zimonqm@hotmail.com>
2021-02-18 19:15:17 +0100

    [changes] App.js added, plus minor shit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Imports components
+import PageGallery from './page/PageGallery';
+import OrganismHeader from './organisms/OrganismHeader';
+
+
+
+function App() {
+	return (
+		<main>
+			<OrganismHeader />
+			<PageGallery />
+		</main>
+	)
+}
+
+export default App;
