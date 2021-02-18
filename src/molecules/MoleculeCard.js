013a5a33590838577b774a790cf1014a8acae27f
Niklas Knudsen <niklasmknudsen@hotmail.com>
2021-02-18 19:21:00 +0100

    [added] moleculeCard component

--- src/molecules/MoleculeCard.js
+++ src/molecules/MoleculeCard.js
@@ -4,9 +4,16 @@ function MoleculeCard(props) {
     return (
         <section class="gallerypage-card">
             <AtomicImage src={this.props.src} alt={this.props.alt} />
+            <Headerline className="textCenter" text={this.props.text} />
         </section>
     );
 }
 
+
+function Headerline(props) {
+    return (
+        <h2>{this.props.text}</h2>
+    );
+}
 
 export default MoleculeCard;
