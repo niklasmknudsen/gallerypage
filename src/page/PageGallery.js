e0561f47a7b7c635e6bf7a679eed27ce90bd6bb1
Niklas Knudsen <niklasmknudsen@hotmail.com>
2021-02-18 16:52:41 +0100

    [removed] App.js since it wasent used

---
+++ src/page/PageGallery.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import AtomicButton from '../atoms/AtomicButton';
+
+class PageGallery extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Atomic Button',
+            backgroundColor: `#` + 191919,
+            color: 'white'
+        };
+        this.styledButton = { backgroundColor: this.state.backgroundColor, color: this.state.color };
+    }
+
+    handleClick = (event) => {
+        console.log('klikker');
+    }
+
+    render() {
+        const { color, backgroundColor } = this.state;
+        return <AtomicButton handleClick={this.handleClick.bind(this)} style={this.styledButton} text={this.state.text} />;
+    }
+}
+
+export default PageGallery;
