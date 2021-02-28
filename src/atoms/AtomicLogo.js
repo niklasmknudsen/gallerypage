dd21312c41097af9a74cbe735e390ea99ea0ab8b
Simon K. Mortensen <zimonqm@hotmail.com>
2021-02-28 21:15:18 +0100

    [Added] Styles and functionality for nav, including logo and mobile menu

---
+++ src/atoms/AtomicLogo.js
@@ -0,0 +1,11 @@
+import React, { Component, useState } from 'react';
+
+function AtomicLogo(props) {
+    return (
+        <div className="logo">
+            <h2>TopTravel</h2>
+        </div>
+    );
+}
+
+export default AtomicLogo;
