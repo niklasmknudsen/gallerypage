1e0b4b519a0a3742e23ea0f3df03b1989f515adf
Niklas Knudsen <niklasmknudsen@hotmail.com>
2021-03-02 00:03:24 +0100

    [added] grid to contact page

--- src/page/PageContact.js
+++ src/page/PageContact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../assets/styles/pages/pagecontact.scss';
 import '.././App.scss';
 import OrganismHeader from '../organisms/OrganismHeader';
+import TemplateContactForm from '../templates/TemplateContactForm';
 
 class PageContact extends React.Component {
 
@@ -13,15 +14,20 @@ class PageContact extends React.Component {
     }
 
 
-
-
-
     render() {
         return (
             <article id="page-contact">
                 <OrganismHeader />
-                <section className=""></section>
-                <section className=""></section>
+                <div class="page-contact__grid">
+                    <section className="grid-column__left">
+                        <TemplateContactForm />
+                    </section>
+                    <section className="grid-column__right">
+                        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                        </iframe>
+                    </section>
+                </div>
             </article>
         );
     };
