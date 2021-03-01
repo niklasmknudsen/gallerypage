08c823505695ab1561485fd8417a79695827dc9b
Niklas Knudsen <niklasmknudsen@hotmail.com>
2021-03-02 00:03:48 +0100

    [added] new component contact form

---
+++ src/templates/TemplateContactForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../assets/styles/templates/contactform.scss';
+
+
+class TemplateContactForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: null,
+            email: null,
+            besked: null
+        }
+    }
+
+
+
+    render() {
+        return (
+            <form className="contact-form">
+                <div className="form-group">
+                    <h2>Contact us</h2>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label><br />
+                    <input type="text" value={this.state.name} className="form-control" name="name" /> 
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label><br/>
+                    <input type="text" className="form-control" value={this.state.email} name="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="besked">Besked</label><br />
+                    <textarea aria-multiline className="form-control"  value={this.state.besked} name="besked"></textarea>
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Send" />
+                </div>
+            </form>
+        );
+    }
+}
+
+export default TemplateContactForm;
